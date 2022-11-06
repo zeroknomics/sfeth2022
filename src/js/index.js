ab9b2397f4cb526854f7d594be5458eccd70ae52
Rodolfo Ruiz <rodolfo@hertzian.io>
2022-11-05 22:40:56 -0700

    button style 2

--- src/js/index.js
+++ src/js/index.js
@@ -7,11 +7,11 @@ const initializeMina = async () => {
     const onboardAuroButton = document.getElementById('connectAuroButton')
 
     onboardAuroButton.onclick = async () => {
-        alert("button clicked")
         if (!window.mina) {
             alert("No Auro Wallet was found!")
         } else {
-            onboardAuroButton.innerText = 'Onboarding...'
+            onboardAuroButton.innerHTML = "<span style='color:white;'>Onboarding...</span>"
+        
             let data = await window.mina.requestAccounts().catch(err => err)
             if (data.message) {
                 onboardAuroButton.innerText = data.message
